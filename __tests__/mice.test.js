f51cf86abc998cdf706fdd412c732a53da918f33
Franco Ortega <francopdx@gmail.com>
2021-02-14 23:55:42 -0800

    add GET test for find method of Mouse model

--- __tests__/mice.test.js
+++ __tests__/mice.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Mouse = require('../lib/models/Mouse');
 
 describe('bonus-lab-crud-app-back-end routes', () => {
   beforeEach(() => {
@@ -27,5 +28,52 @@ describe('bonus-lab-crud-app-back-end routes', () => {
       furColor: 'brown',
       tailLength: 4
     });
+  });
+
+  it('returns all mice via GET', async() => {
+    await Promise.all([
+      {
+        name: 'Pippers',
+        furColor: 'black',
+        tailLength: 3
+      },
+      {
+        name: 'Cassia',
+        furColor: 'tan',
+        tailLength: 2
+      },
+      {
+        name: 'Tuvo',
+        furColor: 'spotted',
+        tailLength: 1
+      }
+    ].map(mouse => Mouse.insert(mouse)));
+
+    const res = await request(app)
+      .get('/api/v1/mice');
+
+    const results = [
+      {
+        id: expect.any(String),
+        name: 'Pippers',
+        furColor: 'black',
+        tailLength: 3
+      },
+      {
+        id: expect.any(String),
+        name: 'Cassia',
+        furColor: 'tan',
+        tailLength: 2
+      },
+      {
+        id: expect.any(String),
+        name: 'Tuvo',
+        furColor: 'spotted',
+        tailLength: 1
+      }
+    ];
+
+    expect(res.body.length).toEqual(results.length);
+    results.forEach(result => expect(res.body).toContainEqual(result));
   });
 });
